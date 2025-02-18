39234b60120f8dc3537948c5e7b8de1276a00065
krishangoyal12 <krishangoyal717@gmail.com>
2025-02-18 16:18:46 +0530

    Completed the assignment

--- db.js
+++ db.js
@@ -2,7 +2,10 @@ const {connect} = require('mongoose')
 
 const connectToDb = async(url)=>{
     try{
-        await connect(url)
+        await connect(url, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
     }
     catch(err){
         console.log("Error in connecting with Database")
